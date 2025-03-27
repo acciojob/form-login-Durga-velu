057e4cf9f5119164b32b2ac90d22503c22f185f8
Durga-velu <durgadevi134babu@gmail.com>
2025-03-27 14:09:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+	let  FirstName=document.querySelector("input[name=fname]").value
+	let   LastName=document.querySelector("input[name=lname]").value
+alert(`${FirstName} ${LastName}` )
 }
